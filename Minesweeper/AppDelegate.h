c7376153572db4cc7578aa91924f8607ecdf9bde
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-10-29 20:16:41 -0700

    Initial Commit

---
+++ Minesweeper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Minesweeper
+//
+//  Created by Zakk Hoyt on 10/29/15.
+//  Copyright © 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
